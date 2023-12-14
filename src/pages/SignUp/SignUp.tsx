be51a9537ca273d89ac2cdd5b1b4701c47861091
〽️iraya <mirayaabrodi@gmail.com>
2023-12-14 20:23:51 +0100

    feat: create signup component

---
+++ src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,60 @@
+import AuthenticationForm from "../../components/AuthenticationForm/AuthenticationForm";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import toast from "react-hot-toast";
+import {
+  firebaseAuth,
+  firebaseFirestore,
+  firebaseStorage,
+} from "../../firebase/firebaseConfig";
+
+export default function SignUp() {
+  const handleSignUp = async (
+    email: string,
+    password: string,
+    fullName?: string,
+    file?: File
+  ) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        firebaseAuth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      let imageUrl = "/empty-avatar.png";
+      if (file) {
+        const fileRef = ref(firebaseStorage, `profilePictures/${user.uid}`);
+        await uploadBytes(fileRef, file);
+        imageUrl = await getDownloadURL(fileRef);
+      }
+
+      await setDoc(doc(firebaseFirestore, "users", user.uid), {
+        fullName: fullName || "",
+        email: email,
+        uid: user.uid,
+        profilePicture: imageUrl,
+      });
+
+      toast.success("Account created successfully!");
+    } catch (error) {
+      toast.error("Failed to create account");
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <AuthenticationForm
+        text={"Sign Up"}
+        extraInput={true}
+        buttonText={"Sign Up"}
+        onSubmit={handleSignUp}
+        linkText={"Already have an account? Sign in here."}
+        linkUrl={"/signin"}
+      />
+    </div>
+  );
+}
