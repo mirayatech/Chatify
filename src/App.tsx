fdf0eb83b20e6634c1c8d730113f07e2f5037aa0
〽️iraya <mirayaabrodi@gmail.com>
2023-12-14 20:24:47 +0100

    add: implement authentication components to app and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,34 @@
+import { Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import { Toaster } from "react-hot-toast";
+
+const SignIn = lazy(() => import("./pages/SignIn/SignIn"));
+const SignUp = lazy(() => import("./pages/SignUp/SignUp"));
+
 export default function App() {
-  return <div>Chatify</div>;
+  return (
+    <div>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Routes>
+        <Route path="/" element={<div>Hello</div>} />
+
+        <Route
+          path="/signin"
+          element={
+            <Suspense fallback={"loading..."}>
+              <SignIn />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Suspense fallback={"loading..."}>
+              <SignUp />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
