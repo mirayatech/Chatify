ab190dd1c8a78432a7e06d7f31c4392f5f9cc0de
〽️iraya <mirayaabrodi@gmail.com>
2023-12-16 11:08:58 +0100

    add: implement Privateroute, listen to user if authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import { Routes, Route } from "react-router-dom";
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 import { AuthContainer } from "./styles/ReusableStyles";
+import { useUserStore } from "./library";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "./firebase/firebaseConfig";
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./pages/Home/Home";
 
 const SignIn = lazy(() => import("./pages/SignIn/SignIn"));
 const SignUp = lazy(() => import("./pages/SignUp/SignUp"));
 
 export default function App() {
+  const { setCurrentUser } = useUserStore();
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (user) => {
+      console.log(user);
+      if (user) {
+        setCurrentUser(user);
+      } else setCurrentUser(null);
+    });
+  }, [setCurrentUser]);
+
   return (
     <div>
       <Toaster position="top-center" reverseOrder={false} />
       <Routes>
-        <Route path="/" element={<div>Hello</div>} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
 
         <Route
           path="/signin"
