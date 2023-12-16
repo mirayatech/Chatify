2134cf0c9b1aac23ffa12bd422f0ac7171bdac0c
〽️iraya <mirayaabrodi@gmail.com>
2023-12-16 21:20:33 +0100

    add: implement home component to app

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
+import { Sidebar } from "../../components/Sidebar/Sidebar";
 import { HomeWrapper, Wrapper, Text } from "./Style";
 
 export default function Home() {
   return (
     <Wrapper>
+      <Sidebar />
       <HomeWrapper>
         <Text>Select a conversation to start chatting.</Text>
       </HomeWrapper>
