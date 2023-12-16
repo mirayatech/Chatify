3291ae043753b84f2050672e6d1df5b19df32237
〽️iraya <mirayaabrodi@gmail.com>
2023-12-16 21:17:07 +0100

    feat: create useCollectionQuery hook

---
+++ src/hooks/useCollectionQuery.ts
@@ -0,0 +1,53 @@
+import type {
+  CollectionReference,
+  DocumentData,
+  Query,
+  QuerySnapshot,
+} from "firebase/firestore";
+
+import { onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const cache: { [key: string]: any } = {};
+
+export const useCollectionQuery: (
+  key: string,
+  collection: CollectionReference | Query<DocumentData>
+) => { loading: boolean; error: boolean; data: QuerySnapshot | null } = (
+  key,
+  collection
+) => {
+  const [data, setData] = useState<QuerySnapshot<DocumentData> | null>(
+    cache[key] || null
+  );
+
+  const [loading, setLoading] = useState(!data);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection,
+      (snapshot) => {
+        setData(snapshot);
+        setLoading(false);
+        setError(false);
+        cache[key] = snapshot;
+      },
+      (err) => {
+        console.log(err);
+        setData(null);
+        setLoading(false);
+        setError(true);
+      }
+    );
+
+    return () => {
+      unsubscribe();
+    };
+
+    // eslint-disable-next-line
+  }, [key]);
+
+  return { loading, error, data };
+};
