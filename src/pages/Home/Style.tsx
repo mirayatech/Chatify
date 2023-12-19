524dfeaf7f22f84383dc0c7c5aa0612a94e444f7
〽️iraya <mirayaabrodi@gmail.com>
2023-12-19 18:28:37 +0100

    style: add lightmode darkmode feature for home component

--- src/pages/Home/Style.tsx
+++ src/pages/Home/Style.tsx
@@ -1,14 +1,17 @@
 import styled from "styled-components";
+import { StyledProps, color } from "../../library";
 
-export const Text = styled.p`
-  font-size: calc(20 / 16 * 1rem);
+export const Text = styled.p<StyledProps>`
+  font-size: calc(18 / 16 * 1rem);
+  color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.text : color.darkMode.text};
 `;
 
 export const Wrapper = styled.div`
   display: flex;
 `;
 
-export const HomeWrapper = styled.div`
+export const HomeWrapper = styled.div<StyledProps>`
   flex-grow: 1;
   display: flex;
   height: 100vh;
@@ -16,7 +19,8 @@ export const HomeWrapper = styled.div`
   align-items: center;
   flex-direction: column;
   justify-content: center;
-  background-color: #f6f6f6;
+  background-color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.background : color.darkMode.background};
 
   @media screen and (max-width: 768px) {
     display: none;
