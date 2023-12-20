94bd8d813b8e2463c9bf94dc3c6604345bcfa7e3
〽️iraya <mirayaabrodi@gmail.com>
2023-12-20 14:38:01 +0100

    feat: create useDocumentQuery hook

---
+++ src/hooks/useDocumentQuery.ts
@@ -0,0 +1,43 @@
+import type {
+  DocumentData,
+  DocumentReference,
+  DocumentSnapshot,
+} from "firebase/firestore";
+
+import { onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+const cache: { [key: string]: DocumentSnapshot<DocumentData> | null } = {};
+
+export const useDocumentQuery = (
+  key: string,
+  document: DocumentReference<DocumentData>
+) => {
+  const [data, setData] = useState<DocumentSnapshot<DocumentData> | null>(
+    cache[key] || null
+  );
+  const [loading, setLoading] = useState(!data);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      document,
+      (snapshot) => {
+        setData(snapshot);
+        setLoading(false);
+      },
+      (err) => {
+        console.log(err);
+        setData(null);
+        setLoading(false);
+        setError(true);
+      }
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, [key]);
+
+  return { loading, error, data };
+};
