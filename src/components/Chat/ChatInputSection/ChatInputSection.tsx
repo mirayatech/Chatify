8c974cf5591fe4429bd99417c4e4c4110919639e
〽️iraya <mirayaabrodi@gmail.com>
2023-12-24 21:00:08 +0100

    feat: cerate ChatInputSection component

---
+++ src/components/Chat/ChatInputSection/ChatInputSection.tsx
@@ -0,0 +1,349 @@
+import type {
+  ChangeEvent,
+  ClipboardEventHandler,
+  FormEvent,
+  SetStateAction,
+} from "react";
+
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { useEffect, useRef, useState } from "react";
+import { BsFillReplyFill } from "react-icons/bs";
+import { HiMiniPaperAirplane } from "react-icons/hi2";
+import { ImAttachment } from "react-icons/im";
+import { RiImageAddFill } from "react-icons/ri";
+import { useParams } from "react-router-dom";
+
+import {
+  Container,
+  Form,
+  ImageButton,
+  FileButton,
+  InputWrapper,
+  Input,
+  SendButton,
+  DragFile,
+  Title,
+  ReplyContainer,
+  ReplyTitle,
+  ReplyText,
+  Text,
+  CloseButton,
+} from "./Style";
+import { useTheme, useUserStore } from "../../../hooks";
+import {
+  firebaseFirestore,
+  firebaseStorage,
+} from "../../../firebase/firebaseConfig";
+import { formatFileName } from "../../../library";
+import { Spinner } from "../../Core";
+import toast from "react-hot-toast";
+import { FiX } from "react-icons/fi";
+
+type InputHeaderProps = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  replyInfo: any;
+  setReplyInfo: React.Dispatch<React.SetStateAction<null>>;
+};
+export function ChatInputSection({
+  replyInfo,
+  setReplyInfo,
+}: InputHeaderProps) {
+  const [inputValue, setInputValue] = useState("");
+  const [fileUploading, setFileUploading] = useState(false);
+  const [previewFiles, setPreviewFiles] = useState<string[]>([]);
+
+  const { currentUser } = useUserStore();
+  const { id: conversationId } = useParams();
+  const { theme } = useTheme();
+
+  const textInputRef = useRef<HTMLInputElement>(null);
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const [fileDragging, setFileDragging] = useState(false);
+
+  const updateTimestamp = () => {
+    updateDoc(
+      doc(firebaseFirestore, "conversations", conversationId as string),
+      {
+        updatedAt: serverTimestamp(),
+      }
+    );
+  };
+
+  useEffect(() => {
+    const handler = () => {
+      textInputRef.current?.focus();
+    };
+    window.addEventListener("focus", handler);
+    return () => window.removeEventListener("focus", handler);
+  }, []);
+
+  useEffect(() => {
+    textInputRef.current?.focus();
+  }, [conversationId]);
+
+  const handleFormSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (previewFiles.length > 0) {
+      setPreviewFiles([]);
+      for (let i = 0; i < previewFiles.length; i++) {
+        const url = previewFiles[i];
+        const res = await fetch(url);
+        const blob = await res.blob();
+        const file = new File([blob], "image.png", {
+          type: res.headers.get("content-type") as string,
+        });
+        await uploadFile(file);
+      }
+    } else {
+      if (fileUploading) return;
+    }
+
+    if (!inputValue.trim()) return;
+
+    setInputValue("");
+
+    const replacedInputValue = ` ${inputValue} `;
+
+    setReplyInfo && setReplyInfo(null);
+
+    addDoc(
+      collection(
+        firebaseFirestore,
+        "conversations",
+        conversationId as string,
+        "messages"
+      ),
+      {
+        sender: currentUser?.uid,
+        content: replacedInputValue.trim(),
+        type: "text",
+        createdAt: serverTimestamp(),
+        replyTo: replyInfo?.id || null,
+      }
+    );
+
+    updateTimestamp();
+  };
+
+  const uploadFile = async (file: File) => {
+    try {
+      const TWENTY_MB = 1024 * 1024 * 20;
+
+      if (file.size > TWENTY_MB) {
+        toast.error("Upload too big. Max 20MB.");
+        return;
+      }
+
+      setFileUploading(true);
+
+      const fileReference = ref(firebaseStorage, formatFileName(file.name));
+
+      await uploadBytes(fileReference, file);
+
+      const downloadURL = await getDownloadURL(fileReference);
+
+      addDoc(
+        collection(
+          firebaseFirestore,
+          "conversations",
+          conversationId as string,
+          "messages"
+        ),
+        {
+          sender: currentUser?.uid,
+          content: downloadURL,
+          type: file.type.startsWith("image") ? "image" : "file",
+          file: file.type.startsWith("image")
+            ? null
+            : {
+                name: file.name,
+                size: file.size,
+              },
+          createdAt: serverTimestamp(),
+        }
+      );
+
+      setFileUploading(false);
+      updateTimestamp();
+    } catch (error) {
+      console.log(error);
+      setFileUploading(false);
+    }
+  };
+
+  const handleFileInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (!file) return;
+
+    uploadFile(file);
+  };
+
+  const handlePaste: ClipboardEventHandler<HTMLInputElement> = (event) => {
+    const file = event?.clipboardData?.files?.[0];
+    if (!file || !file.type.startsWith("image")) return;
+
+    const url = URL.createObjectURL(file);
+
+    setPreviewFiles([...previewFiles, url]);
+  };
+
+  useEffect(() => {
+    const dragBlurHandler = (event: DragEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
+      setFileDragging(false);
+    };
+
+    const dragFocusHandler = (event: DragEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
+      setFileDragging(true);
+    };
+
+    const dropFileHandler = async (event: DragEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      setFileDragging(false);
+
+      const items = event?.dataTransfer?.items;
+      const files = event?.dataTransfer?.files;
+
+      const selectedFiles = [];
+
+      if (items) {
+        for (let i = 0; i < items.length; i++) {
+          const item = items[i];
+          if (item) {
+            const entry = item.webkitGetAsEntry();
+            if (entry && entry.isFile && files) {
+              selectedFiles.push(files[i]);
+            }
+          }
+        }
+      }
+
+      for (let i = 0; i < selectedFiles.length; i++) {
+        await uploadFile(selectedFiles[i]);
+      }
+    };
+
+    addEventListener("dragenter", dragFocusHandler);
+    addEventListener("dragover", dragFocusHandler);
+    addEventListener("dragleave", dragBlurHandler);
+    addEventListener("drop", dropFileHandler);
+
+    return () => {
+      removeEventListener("dragenter", dragFocusHandler);
+      removeEventListener("dragover", dragFocusHandler);
+      removeEventListener("dragleave", dragBlurHandler);
+      removeEventListener("drop", dropFileHandler);
+    };
+  }, []);
+  return (
+    <>
+      {fileDragging && (
+        <DragFile>
+          <Title>Drop file to send</Title>
+        </DragFile>
+      )}
+
+      {previewFiles.length === 0 && !!replyInfo && (
+        <ReplyContainer theme={theme}>
+          <div>
+            <ReplyTitle theme={theme}>
+              <BsFillReplyFill />
+              <p>
+                Replying
+                {currentUser?.uid === replyInfo.sender ? " to yourself" : ""}
+              </p>
+            </ReplyTitle>
+            {replyInfo.type === "text" ? (
+              <Text theme={theme}>{replyInfo.content}</Text>
+            ) : replyInfo.type === "image" ? (
+              <ReplyText theme={theme}>An image</ReplyText>
+            ) : replyInfo.type === "file" ? (
+              <ReplyText theme={theme}>A file</ReplyText>
+            ) : (
+              "Message has been removed"
+            )}
+          </div>
+
+          <CloseButton
+            theme={theme}
+            onClick={() => setReplyInfo && setReplyInfo(null)}
+          >
+            <FiX style={{ fontSize: "1rem" }} />
+          </CloseButton>
+        </ReplyContainer>
+      )}
+      <Container theme={theme}>
+        <ImageButton
+          theme={theme}
+          onClick={() => imageInputRef.current?.click()}
+        >
+          <RiImageAddFill />
+        </ImageButton>
+        <input
+          ref={imageInputRef}
+          hidden
+          type="file"
+          accept="image/*"
+          onChange={handleFileInputChange}
+        />
+        <FileButton theme={theme} onClick={() => fileInputRef.current?.click()}>
+          <ImAttachment />
+        </FileButton>
+        <input
+          ref={fileInputRef}
+          hidden
+          className="hidden"
+          type="file"
+          onChange={handleFileInputChange}
+        />
+
+        <Form onSubmit={handleFormSubmit}>
+          <InputWrapper>
+            <Input
+              theme={theme}
+              maxLength={1000}
+              ref={textInputRef}
+              value={inputValue}
+              onChange={(event: {
+                target: { value: SetStateAction<string> };
+              }) => {
+                setInputValue(event.target.value);
+              }}
+              onPaste={handlePaste}
+              type="text"
+              placeholder="Message..."
+            />
+          </InputWrapper>
+          {fileUploading ? (
+            <SendButton>
+              <Spinner />
+            </SendButton>
+          ) : (
+            <SendButton
+              theme={theme}
+              disabled={!inputValue.trim() || fileUploading}
+            >
+              <HiMiniPaperAirplane />
+            </SendButton>
+          )}
+        </Form>
+      </Container>
+    </>
+  );
+}
---
+++ src/components/Chat/ChatInputSection/Style.tsx
@@ -0,0 +1,164 @@
+import styled from "styled-components";
+import { StyledProps, color } from "../../../library";
+
+export const Container = styled.div<StyledProps>`
+  display: flex;
+  padding: 15px 20px;
+  align-items: center;
+  justify-content: center;
+  border-top: 1px solid
+    ${({ theme }) =>
+      theme === "light" ? color.lightMode.border : color.darkMode.border};
+  background-color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.background : color.darkMode.background};
+`;
+
+export const Form = styled.form`
+  gap: 1;
+  flex-grow: 1;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const ImageButton = styled.button<StyledProps>`
+  border: none;
+  display: flex;
+  font-size: 1.5rem;
+  align-items: center;
+  background: transparent;
+  color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.text : color.darkMode.text};
+`;
+
+export const FileButton = styled.button<StyledProps>`
+  border: none;
+  display: flex;
+  padding: 0 15px;
+  font-size: 1.3rem;
+  align-items: center;
+  background: transparent;
+  color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.text : color.darkMode.text};
+`;
+
+export const InputWrapper = styled.div`
+  flex-grow: 1;
+  display: flex;
+  position: relative;
+  align-items: center;
+`;
+
+export const Input = styled.input<StyledProps>`
+  width: 100%;
+  border: none;
+  outline: none;
+  font-size: 1rem;
+  padding: 10px 15px;
+  border-radius: 30px;
+  background-color: ${({ theme }) =>
+    theme === "light"
+      ? color.lightMode.chatBackground
+      : color.darkMode.chatBackground};
+  border: 1px solid
+    ${({ theme }) =>
+      theme === "light"
+        ? color.lightMode.chatBackground
+        : color.darkMode.chatBackground};
+
+  color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.title : color.darkMode.title};
+
+  ::placeholder {
+    color: ${({ theme }) =>
+      theme === "light" ? color.lightMode.text : color.darkMode.text};
+  }
+`;
+
+export const CloseButton = styled.button<StyledProps>`
+  top: 10px;
+  right: 10px;
+  border: none;
+  position: absolute;
+  background-color: transparent;
+  color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.title : color.darkMode.title};
+`;
+
+export const SendButton = styled.button<StyledProps>`
+  border: none;
+  display: flex;
+  font-size: 1.6rem;
+  padding-left: 10px;
+  align-items: center;
+  background: transparent;
+  color: ${({ theme }) => (theme === "light" ? color.primary : color.primary)};
+  &:disabled {
+    color: ${({ theme }) =>
+      theme === "light" ? color.lightMode.text : color.darkMode.text};
+  }
+`;
+
+export const DragFile = styled.div`
+  z-index: 20;
+  width: 100vh;
+  display: flex;
+  height: 100vh;
+  position: fixed;
+  user-select: none;
+  align-items: center;
+  pointer-events: none;
+  justify-content: center;
+  transition: all 0.2s ease;
+
+  background-color: #3333331b;
+`;
+
+export const Title = styled.div`
+  z-index: 30;
+  color: black;
+  font-size: calc(30 / 16 * 1rem);
+`;
+
+export const ReplyContainer = styled.div<StyledProps>`
+  display: flex;
+  position: relative;
+  align-items: center;
+  padding: 15px 30px;
+  justify-content: space-between;
+
+  background-color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.background : color.darkMode.background};
+  border-top: 1px solid
+    ${({ theme }) =>
+      theme === "light" ? color.lightMode.border : color.darkMode.border};
+`;
+
+export const ReplyTitle = styled.div<StyledProps>`
+  display: flex;
+  font-weight: 500;
+  color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.title : color.darkMode.title};
+  align-items: baseline;
+  padding-bottom: 10px;
+  gap: 5px;
+  svg {
+    font-size: 1.1rem;
+  }
+`;
+
+export const ReplyText = styled.p<StyledProps>`
+  color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.text : color.darkMode.text};
+`;
+
+export const Text = styled.p<StyledProps>`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  color: darkgray;
+  max-width: calc(100vw - 420px);
+
+  @media screen and (max-width: 768px) {
+    max-width: calc(100vw - 65px);
+  }
+`;
