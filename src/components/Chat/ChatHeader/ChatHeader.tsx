e2135659ebf096219fef62c4e06afafaa1c40da3
〽️iraya <mirayaabrodi@gmail.com>
2023-12-27 14:52:11 +0100

    feat: create ChatHeader component

---
+++ src/components/Chat/ChatHeader/ChatHeader.tsx
@@ -0,0 +1,153 @@
+import { Skeleton } from "@mui/material";
+import { useState } from "react";
+import { FaChevronLeft } from "react-icons/fa6";
+import { MdGroups, MdInfo } from "react-icons/md";
+
+import { useTheme, useUserStore, useUsersInfo } from "../../../hooks";
+import { ConversationInfoType, IMAGE_PROXY } from "../../../library";
+import {
+  Header,
+  Wrapper,
+  SingleImage,
+  Relative,
+  ImagePrimary,
+  ImageSecondary,
+  Name,
+  GroupButton,
+  SettingButton,
+  HomeLink,
+} from "./Style";
+import { ChatConversationSettings } from "..";
+import { ChatViewGroup } from "../ChatViewGroup/ChatViewGroup";
+import { ViewMedia } from "../../Media/ViewMedia";
+
+type ChatHeaderProps = {
+  conversation: ConversationInfoType;
+};
+export function ChatHeader({ conversation }: ChatHeaderProps) {
+  const { data: users, loading } = useUsersInfo(conversation.users);
+  const { currentUser } = useUserStore();
+  const { theme } = useTheme();
+  const filtered = users?.filter((user) => user.id !== currentUser?.uid);
+
+  const [isConversationSettingsOpen, setIsConversationSettingsOpen] =
+    useState(false);
+
+  const [isGroupMembersOpen, setIsGroupMembersOpen] = useState(false);
+  const [isViewMediaOpen, setIsViewMediaOpen] = useState(false);
+
+  return (
+    <>
+      {theme && (
+        <Header theme={theme}>
+          <Wrapper>
+            <HomeLink
+              theme={theme}
+              className="mobile__link"
+              to="/"
+              aria-label="Home"
+            >
+              <FaChevronLeft />
+            </HomeLink>
+
+            {loading ? (
+              <Skeleton variant="circular" width={40} height={40} />
+            ) : (
+              <>
+                {conversation.users.length === 2 ? (
+                  <SingleImage
+                    src={IMAGE_PROXY(filtered?.[0]?.data()?.photoURL)}
+                    alt=""
+                  />
+                ) : (
+                  <>
+                    {conversation?.group?.groupImage ? (
+                      <SingleImage src={conversation.group.groupImage} alt="" />
+                    ) : (
+                      <Relative>
+                        <ImagePrimary
+                          theme={theme}
+                          src={IMAGE_PROXY(filtered?.[0]?.data()?.photoURL)}
+                          alt=""
+                        />
+                        <ImageSecondary
+                          src={IMAGE_PROXY(filtered?.[1]?.data()?.photoURL)}
+                          alt=""
+                        />
+                      </Relative>
+                    )}
+                  </>
+                )}
+              </>
+            )}
+
+            {loading ? (
+              <Skeleton
+                width={100}
+                height={15}
+                variant="rectangular"
+                sx={{ ml: "10px" }}
+              />
+            ) : (
+              <Name theme={theme}>
+                {conversation.users.length > 2 && conversation?.group?.groupName
+                  ? conversation.group.groupName
+                  : filtered
+                      ?.map((user) => user.data()?.displayName)
+                      .slice(0, 3)
+                      .join(", ")}
+              </Name>
+            )}
+          </Wrapper>
+
+          {!loading && (
+            <Wrapper>
+              {conversation.users.length > 2 && (
+                <GroupButton
+                  theme={theme}
+                  onClick={() => setIsGroupMembersOpen(true)}
+                >
+                  <MdGroups />
+                </GroupButton>
+              )}
+
+              <SettingButton
+                theme={theme}
+                onClick={() => setIsConversationSettingsOpen(true)}
+              >
+                <MdInfo />
+              </SettingButton>
+            </Wrapper>
+          )}
+        </Header>
+      )}
+
+      {isConversationSettingsOpen && theme && (
+        <ChatConversationSettings
+          theme={theme}
+          conversation={conversation}
+          setMediaViewOpen={setIsViewMediaOpen}
+          isOpen={isConversationSettingsOpen}
+          setIsOpen={setIsConversationSettingsOpen}
+        />
+      )}
+
+      {isGroupMembersOpen && theme && (
+        <ChatViewGroup
+          theme={theme}
+          conversation={conversation}
+          isOpen={isGroupMembersOpen}
+          setIsOpen={setIsGroupMembersOpen}
+        />
+      )}
+
+      {isViewMediaOpen && theme && (
+        <ViewMedia
+          theme={theme}
+          isOpen={isViewMediaOpen}
+          setIsOpen={setIsViewMediaOpen}
+        />
+      )}
+    </>
+  );
+}
---
+++ src/components/Chat/ChatHeader/Style.tsx
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import { StyledProps, color } from "../../../library";
+import { Link } from "react-router-dom";
+
+export const Header = styled.div<StyledProps>`
+  height: 84px;
+  display: flex;
+  padding: 020px;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid
+    ${({ theme }) =>
+      theme === "light" ? color.lightMode.border : color.darkMode.border};
+  background-color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.background : color.darkMode.background};
+`;
+export const Name = styled.p<StyledProps>`
+  font-weight: 500;
+  margin-left: 15px;
+  color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.title : color.darkMode.title};
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const HomeLink = styled(Link)<StyledProps>`
+  display: flex;
+  font-size: 1.4rem;
+  margin-right: 20px;
+  color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.text : color.darkMode.text};
+`;
+
+export const SingleImage = styled.img`
+  width: 40px;
+  border-radius: 50%;
+`;
+export const Relative = styled.div`
+  margin: 0 15px;
+  position: relative;
+  padding-right: 10px;
+`;
+
+export const ImagePrimary = styled.img<StyledProps>`
+  top: -5px;
+  left: -30px;
+  z-index: 1;
+  width: 30px;
+  padding: 1px;
+  display: flex;
+  border-radius: 50%;
+  position: absolute;
+  align-items: center;
+  margin: 0 10px 2px 10px;
+  border: double 1px transparent;
+  background-clip: content-box, border-box;
+
+  background-image: ${({ theme }) =>
+    theme === "light"
+      ? `linear-gradient(white, white),
+    radial-gradient(circle at 20% 107%, ${color.white} 0%, ${color.white} 60%);`
+      : `linear-gradient(white, white),
+    radial-gradient(circle at 20% 107%, ${color.darkMode.background}   0%, ${color.darkMode.background}  60%);`};
+`;
+export const ImageSecondary = styled.img`
+  top: -19px;
+  left: -8px;
+  width: 30px;
+  border-radius: 50%;
+  position: absolute;
+`;
+
+export const SettingButton = styled.button<StyledProps>`
+  border: none;
+  display: flex;
+  font-size: 1.5rem;
+  margin-left: 30px;
+  color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.text : color.darkMode.text};
+  background-color: transparent;
+`;
+
+export const GroupButton = styled.button<StyledProps>`
+  border: none;
+  display: flex;
+  font-size: 2rem;
+  margin-left: 30px;
+  color: ${({ theme }) =>
+    theme === "light" ? color.lightMode.text : color.darkMode.text};
+  background-color: transparent;
+`;
