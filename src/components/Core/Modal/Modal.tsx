a9574736555bd0b1f79f0fd2a98fc06222f6e61f
〽️iraya <mirayaabrodi@gmail.com>
2023-12-28 18:16:31 +0100

    style: give modal a max-height

--- src/components/Core/Modal/Modal.tsx
+++ src/components/Core/Modal/Modal.tsx
@@ -37,6 +37,7 @@ export const Modal = ({
           padding: "30px 10px 10px",
           minWidth: "320px",
           maxWidth: "500px",
+          maxHeight: "350px",
           margin: "0 5px",
           borderRadius: "10px",
         },
